42c11ce28ec4936648eb2d02cdb7f6ed260d6258
Mahmoud <mymafm0405@gmail.com>
2020-11-25 11:46:33 +0300

    Adding ingredients working now

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onAddedIngredient(ingredientData: Ingredient) {
+    this.ingredients.push(ingredientData);
+  }
+
 }
