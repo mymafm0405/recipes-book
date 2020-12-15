83a95f396be8ca293dc7b17943ca4d8150d1b604
Mahmoud <mymafm0405@gmail.com>
2020-12-15 19:14:53 +0300

    Now edit recipe is working except the save and cancel buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -36,6 +36,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,16 +12,58 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
+  currentRecipe: Recipe;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipesService: RecipesService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.currentRecipe = this.recipesService.getRecipe(this.id);
+        this.initForm();
       }
     )
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let imagePath = '';
+    let description = '';
+    let ingredients = new FormArray([]);
+
+    if (this.editMode) {
+      recipeName = this.currentRecipe.name;
+      imagePath = this.currentRecipe.imageUrl;
+      description = this.currentRecipe.description;
+      if (this.currentRecipe['ingredients']) {
+        for (let ing of this.currentRecipe.ingredients) {
+          ingredients.push(
+            new FormGroup({
+              'name': new FormControl(ing.name),
+              'amount': new FormControl(ing.amount)
+            })
+          )
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(description),
+      'ingredients': ingredients
+    })
+  }
+
+  getIngControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm.value);
   }
 
 }
